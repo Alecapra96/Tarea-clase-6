725dea1d04d1460197b9057bb91cce53d72679ce
alecapra96 <alejandrocapraargentina@gmail.com>
2021-01-14 13:44:18 -0300

    mejore codigo y cambie css

--- grupoFamiliar.js
+++ grupoFamiliar.js
@@ -1,39 +1,50 @@
 let $buttonNext = document.getElementById("buttonNext");
-let $placeImputs=document.getElementById("secondPart");
-// let $buttonNext2=document.getElementById("click-button-next");
+let $placeImputs=document.getElementById("second-part");
 let $buttonMore;
 let arrayGenerado = [] ;
+let num=0;
+let numeroMayor;
+let numeroMenor;
+
 $buttonNext.addEventListener("click",ocultarInicio);
 $buttonNext.addEventListener("click",ShowsInputsCreated);
 
 createButtonMore();  // Aca invoco la funcion de crear los botones
 createButtonNext2(); // llamo la funcion de crear al boton que calcula el promedio
 
-$buttonMore.addEventListener("click",CalcularPromedio); 
+$buttonMore.addEventListener("click",CalculateAges); 
+
+
 
 function ShowsInputsCreated(){ //Muestra imputs de acuerdo al numero que escribio el cliente
     event.preventDefault();  //Esto lo pongo para que no pase lo que paso en clase-5
     document.querySelector("#titulo").innerText = "Promedio de edad de tus familiares"; // Cambio el titulo
 
 
-
-//ACA TENGO QUE HACER QUE DEJEN DE ESTAR OCULTOS LOS BOTONES DEL A FUNCION CREATEBUTTONMORE Y CREATEBUTTONNEXT2
-
-
-
     let $numberFamily = document.querySelector("#numberFamily").value; // Selecciono el valor que introdujo el usuario del numero de familiares
 
-    if ($numberFamily != 0){ // Creo tantos imputs como $numberFamily tenga.
+    if ($numberFamily > 0 && $numberFamily <= 15){ // Creo tantos imputs como $numberFamily tenga.
 
         for(i=0;i<$numberFamily;i++){
             createInput();
         }   
     }
+    else if ($numberFamily >15){
+        alert("Dudo que su padre tenga mas de 15 hijos");
+        location.reload()
+    }
+    else{
+        alert("Escriba mejor");
+        location.reload()
+    }
 } 
 
-function CalcularPromedio(){
+function CalculateAges(){
+    event.preventDefault();
+
     let verArray= [] ;
     let $imputsGenerados = Array.from(document.getElementsByClassName("inputsGenerados"));
+    if ($imputsGenerados != 0){ //POR QUE NO FUNCIONA ESTE IF?
     
      for (let i=0;i<$imputsGenerados.length;i++)
      {
@@ -41,13 +52,19 @@ function CalcularPromedio(){
         arrayGenerado.push(verArray.value);
     }
     console.log(arrayGenerado);
-    CalcularMayor();
-
-    
-    
+    CalculateHigher();
+    CalculateLess();
+    calculateAverage()
+    showResults();
+    document.getElementById('labelResults').innerHTML = 'El numero mas grande es '+numeroMayor+", el numero menor es "+numeroMenor+ " y el promedio es "+num;
+    }
+    else{
+        alert("ingresa mejor los datos");
+    }
 }
-function CalcularMayor(){
-    let numeroMayor=Number(arrayGenerado[0]);
+
+function CalculateHigher(){
+    numeroMayor=arrayGenerado[0];
 
     for (let i=0;i<arrayGenerado.length;i++)
     {
@@ -58,26 +75,52 @@ function CalcularMayor(){
         }
     }
     console.log(numeroMayor);
-
+    return numeroMayor;
 }
 
+function CalculateLess(){
+    numeroMenor=arrayGenerado[0];
+
+    for (let i=0;i<arrayGenerado.length;i++)
+    {
+
+        if (Number(arrayGenerado[i]) < numeroMenor ) {
+            numeroMenor = arrayGenerado[i];
+
+        }
+    }
+    console.log(numeroMenor);
+    return numeroMenor;
+}
+
+function calculateAverage(){
+    for (let i=0;i<arrayGenerado.length;i++)
+    {
+        num += (Number(arrayGenerado[i]));
+    }
+    num = num / arrayGenerado.length;
+    console.log(num);
+    return num;
+}
 
 function ocultarInicio() { // Funcion para ocultar el imput y el button del principio
     event.preventDefault();
     document.getElementById("numberFamily").style.display = "none";
     document.getElementById("buttonNext").style.display = "none";
+    document.getElementById("click-button-more").style.display = "flex";
+    document.getElementById("click-button-next").style.display = "flex";
 }
 
 function createInput(){
     const $imputFamily = document.createElement('input');
-    $imputFamily.placeholder = "Ingresa la edad del miembro ";
+    $imputFamily.placeholder = "Edad del miembro ";
     $imputFamily.className = "inputsGenerados";
     $imputFamily.type = "number";
     $placeImputs.appendChild($imputFamily);
 }
 
 function createButtonMore(){ // Agrego un boton para sumar o restar integrantes
-    let $placeImputs1=document.getElementById("thirdPart");
+    let $placeImputs1=document.getElementById("third-part");
     const $buttonMore = document.createElement('BUTTON');
     $buttonMore.id = "click-button-more";
     $buttonMore.innerHTML = "+"; 
@@ -89,10 +132,18 @@ function createButtonMore(){ // Agrego un boton para sumar o restar integrantes
 
 function createButtonNext2(){ // Agrego un boton para calcular lo que el usuario introdujo en los imputs generados con la funcion sumarImputs()
 
-    let $placeImputs1=document.getElementById("fourPart");
+    let $placeImputs1=document.getElementById("four-part");
     $buttonMore = document.createElement('BUTTON');
     $buttonMore.id = "click-button-next";
     $buttonMore.innerHTML = "CALCULAR!"; 
     $placeImputs1.appendChild($buttonMore);
     return $buttonMore;
+}
+function showResults(){
+    let $placeImputs1=document.getElementById("five-part");
+    $label = document.createElement('LABEL');
+    $label.id = "labelResults";
+    
+    $placeImputs1.appendChild($label);
+    return $label;
 }
